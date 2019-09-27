3df9a74ef6ef397eff20cddc49c01ac0d4140e3d
visual_community@outlook.com <visual_community@outlook.com>
2019-09-27 14:32:19 -0400

    Modelo

---
+++ WebEval/WebEval/Models/Estudiante.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace WebEval.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum Gender
+    {
+        Masculino,
+        Femenino
+    }
+    public class Estudiante
+    {
+        [Key]
+        public int StudentID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public Gender gender { get; set; }
+        [Required]
+        public string Date { get; set; }
+    }
+}
---
+++ WebEval/WebEval/Models/Nota.cs
@@ -0,0 +1,23 @@
+﻿
+namespace WebEval.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum Estado
+    {
+        Aprobado,
+        Reprobado
+    }
+    public class Nota
+    {
+        [Key]
+        [Required]
+        public int Calificacion { get; set; }
+        [Required]
+        public int StudentID { get; set; }
+        [Required]
+        public Estado estado { get; set; }
+        [Required]
+        public string Materia { get; set; }
+    }
+}
